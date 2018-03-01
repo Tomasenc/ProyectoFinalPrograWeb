50999b69b99d656c23f027bed13139de1e099203
Tomás Navarro <tomasenc@hotmail.es>
2018-02-28 21:50:30 -0600

    Last Update 28/02/18

---
+++ POS/POS/POS_Data/MUserName.cs
@@ -0,0 +1,45 @@
+﻿using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POS_BL;
+using POS_Entitiy;
+using POS_Data;
+
+namespace POS_DATA
+{
+    public class MUserName : IUserName
+    {
+        private OrmLiteConnectionFactory _conexion;
+        private IDbConnection _db;
+
+        public MUserName()
+        {
+            _conexion = new OrmLiteConnectionFactory(DBAccess.Default.connection, SqlServerDialect.Provider);
+            _db = _conexion.Open();
+        }
+
+        public void AddUserName(UserName pUserName)
+        {
+            _db.Insert(pUserName);
+        }
+
+        public UserName GetUserName(string pUserName)
+        {
+            return _db.Select<UserName>(x => x.Username == pUserName).FirstOrDefault();
+        }
+
+        public void ModifyUserName(UserName pUsername)
+        {
+            _db.Update(pUsername);
+        }
+        
+        public List<UserName> UserNameList()
+        {
+            return _db.Select<UserName>();
+        }
+    }
+}
---
+++ POS/POS/POS_UI/Login.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="POS_UI.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>POS-Restaurant | Login </title>
+    <link href="css/bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        his is a test
+    </div>
+    </form>
+</body>
+</html>
